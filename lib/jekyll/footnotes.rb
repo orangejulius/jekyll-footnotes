3608f2a02f57d7e89a4be44fc83d8a4bbba943e4
Julian Simioni <julian.simioni@gmail.com>
2015-02-01 08:29:22 +0100

    Create jekyll-footnotes gem

---
+++ lib/jekyll/footnotes.rb
@@ -0,0 +1,7 @@
+require "jekyll/footnotes/version"
+
+module Jekyll
+  module Footnotes
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/footnotes/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module Footnotes
+    VERSION = "0.0.1"
+  end
+end
