380e8f9199d38569c343c850ac28c3f3bf5602c5
Julian Simioni <julian.simioni@gmail.com>
2015-02-01 08:53:31 +0100

    Move code to Gem file

--- lib/jekyll/footnotes.rb
+++ lib/jekyll/footnotes.rb
@@ -1,7 +1,53 @@
 require "jekyll/footnotes/version"
 
 module Jekyll
-  module Footnotes
-    # Your code goes here...
+  class FootnoteTag < Liquid::Tag
+    def initialize(tag_name, id, tokens)
+      raise(SyntaxError.new("invalid footnote ID")) if ['"', '<', '>'].any? { |c| id.include?(c) }
+      @id = id.strip unless id.strip.empty?
+      super
+    end
+
+    def render(context)
+      if @id.nil?
+        context.registers[:fn] ||= 0
+        context.registers[:fn] = context.registers[:fn].next
+        @id = context.registers[:fn]
+      end
+      "<sup><a href=\"#fn:#{@id}\" class=\"footnote\" rel=\"footnote\">#{@id}</a></sup>"
+    end
   end
+
+  class FootnoteBody < Liquid::Block
+    def initialize(tag_name, id, tokens)
+      raise(SyntaxError.new("invalid footnote ID")) if ['"', '<', '>'].any? { |c| id.include?(c) }
+      @id = id.strip unless id.strip.empty?
+      super
+    end
+
+    def render(context)
+      if @id.nil?
+        context.registers[:fnbody] ||= 0
+        context.registers[:fnbody] = context.registers[:fnbody].next
+        @id = context.registers[:fnbody]
+      end
+      context.stack do
+        body = super
+        "<li id=\"fn:#{@id}\" class=\"footnotebody\"value=\"#{@id}\">#{body}</li>"
+      end
+    end
+  end
+
+  class FootnoteList < Liquid::Block
+    def render(context)
+      context.stack do
+        body = super
+        "<ol class=\"footnotelist\">#{body}</ol>"
+      end
+    end
+  end
+
+  Liquid::Template.register_tag('fn', Jekyll::FootnoteTag)
+  Liquid::Template.register_tag('footnotes', FootnoteList)
+  Liquid::Template.register_tag('fnbody', FootnoteBody)
 end
